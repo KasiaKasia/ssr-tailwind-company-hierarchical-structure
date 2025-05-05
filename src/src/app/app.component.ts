b067b6f1bd9d1d01c58936a21a43d970ad28bff5
--add <bulicka.alexandra@gmail.com>
2025-05-05 15:22:33 +0200

    [feat] - replacing the zone.js library with the provideExperimentalZonelessChangeDetection() method

---
+++ src/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './core/navbar/navbar.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { NavbarWcagComponent } from './core/navbar-wcag/navbar-wcag.component';
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, NavbarComponent, NavbarWcagComponent, FooterComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
+})
+export class AppComponent {}
