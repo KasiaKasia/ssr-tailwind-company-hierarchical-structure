6ab0ef0a1c09885c2f9fcb9a16588a749aabeed5
--add <bulicka.alexandra@gmail.com>
2025-05-10 22:44:14 +0200

    [refactor] - component name refactoring

--- src/app/employees/employee-list/employee-list.component.ts
+++ src/app/employees/employee-list/employee-list.component.ts
@@ -1,16 +1,16 @@
 import { Component, inject } from '@angular/core';
 import { EmployeesService } from '../../core/services/employees.service';
 import { Employee, EmployeeStructure } from '../../core/services/interfaces/employee';
-import { EmployeeDetailsComponent } from '../employee-details/employee-details.component';
 import { FormsModule } from '@angular/forms';
 import { SelectComponent } from '../../shared/components/select/select.component';
 import { TreeNodeComponent } from '../../shared/components/tree-node/tree-node.component';
+import { EmployeeNestedListComponent } from '../employee-nested-list/employee-nested-list.component';
 
 
 @Component({
   selector: 'app-employee-list',
   standalone: true,
-  imports: [EmployeeDetailsComponent, FormsModule, SelectComponent, TreeNodeComponent ],
+  imports: [EmployeeNestedListComponent, FormsModule, SelectComponent, TreeNodeComponent ],
   templateUrl: './employee-list.component.html',
   styleUrl: './employee-list.component.css'
 })
--- src/app/employees/employee-nested-list/employee-nested-list.component.ts
+++ src/app/employees/employee-nested-list/employee-nested-list.component.ts
@@ -1,15 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { EmployeeStructure } from '../../core/services/interfaces/employee';
-import { NgFor } from '@angular/common';
 
 @Component({
-  selector: 'app-employee-details',
+  selector: 'app-employee-nested-list',
   standalone: true,
-  imports: [NgFor],
-  templateUrl: './employee-details.component.html',
-  styleUrl: './employee-details.component.css'
+  templateUrl: './employee-nested-list.component.html',
+  styleUrl: './employee-nested-list.component.css'
 })
-export class EmployeeDetailsComponent {
+export class EmployeeNestedListComponent {
   @Input() node!: EmployeeStructure;
   
   getChildren(): EmployeeStructure | EmployeeStructure[] | null {
